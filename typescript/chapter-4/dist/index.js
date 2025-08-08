1f9488afe57e09de10641c433aa6e84080bf0eb9
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-08 21:27:33 +0530

    chapter-4

---
+++ typescript/chapter-4/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("Hello");
+const user1 = {
+    name: "Sigma",
+    age: 21,
+    isAdmin: true
+};
+const greet = (name) => {
+    return `Hello, ${name}`;
+};
+const names = ["alpha", "beta", "gamma"];
+const test = {
+    a: "Hello",
+    b: 11,
+    c: true
+};
